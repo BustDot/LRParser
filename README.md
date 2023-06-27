b04add31bdbd0d91973e066d900c71d9d2845cc1
BustDot <Bust.dev@outlook.com>
2023-06-27 22:59:32 +0800

    feat[LR1]: init LR1Parser

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a grammar parser which implements SLR1 and LR1.
 
 ## SLR1Parser
+
+## LR1Parser
---
+++ src/include/LR1Parser.h
@@ -0,0 +1,78 @@
+#ifndef LRPARSER_LR1PARSER_H
+#define LRPARSER_LR1PARSER_H
+
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+namespace LR1 {
+    struct Production {
+        char left;
+        vector<char> rigths;
+
+        /* 重载== */
+        bool operator==(Production &rhs) const {
+            if (left != rhs.left)
+                return false;
+            for (int i = 0; i < rigths.size(); i++) {
+                if (i >= rhs.rigths.size())
+                    return false;
+                if (rigths[i] != rhs.rigths[i])
+                    return false;
+            }
+            return true;
+        }
+    };
+
+/* LR1项目 */
+    struct LR1Item {
+        Production p;
+        /* 点的位置 */
+        int location;
+        /* 向前看符号 */
+        char next;
+    };
+
+/* LR1项目集 */
+    struct LR1Items {
+        vector<LR1Item> items;
+    };
+
+/* LR1项目集规范族 */
+    struct CanonicalCollection {
+        /* 项目集集合 */
+        vector<LR1Items> items;
+        /* 保存DFA的图，first为转移到的状态序号，second是经什么转移 */
+        vector<pair<int, char> > g[100];
+    };
+
+/* 文法结构体 */
+    struct Grammar {
+        int num;  // 产生式数量
+        vector<char> T;   // 终结符
+        vector<char> N;   // 非终结符
+        vector<Production> prods;  //产生式
+    };
+
+    class LR1Parser {
+    public:
+        string str;
+        CanonicalCollection CC;
+        Grammar grammar;
+        map<char, set<char>> first;
+        map<char, set<char>> follow;
+        queue<pair<LR1Items, int>> Q; // DFA队列， 用于存储待转移的有效项目集
+        pair<int, int> action[100][100]; // first表示分析动作，0->ACC 1->S 2->R second表示转移状态或者产生式序号
+        int goton[100][100];
+        stack<pair<int, char>> ST; // state symbol
+
+    public:
+
+    };
+}
+#endif //LRPARSER_LR1PARSER_H
